633afa623189f2d82bbffd55cfbff191a243fc7c
Artur Cozzi <80040130+ArturCozzi@users.noreply.github.com>
2023-04-13 19:25:39 -0300

    Criar Gerente e Vendedor
    Criar classe gerente e vendedor

---
+++ AtividadePoo/src/Models/Gerente.java
@@ -0,0 +1,23 @@
+ package Models;
+ public class Gerente extends Models.Empregado {
+   
+   private java.lang.String setor;
+   
+   public Gerente(java.lang.String nome, java.lang.String cpf, double salario, java.lang.String setor) {
+   }
+   
+   public java.lang.String getSetor() {
+     return null;
+   }
+   
+   public void setSetor(java.lang.String setor) {
+   }
+   
+   public java.lang.String toString() {
+     return null;
+   }
+   
+   public double calcularPagamento() {
+     return 0;
+   }
+ }
---
+++ AtividadePoo/src/Models/Vendedor.java
@@ -0,0 +1,21 @@
+
+ package Models;
+ public class Vendedor extends Models.Empregado {
+   
+   private double comissao;
+   
+   public Vendedor(java.lang.String nome, java.lang.String cpf, double salario, double comissao) {
+   }
+   
+   public double getComissao() {
+     return 0;
+   }
+   
+   public double calcularPagamento() {
+     return 0;
+   }
+   
+   public java.lang.String toString() {
+     return null;
+   }
+ }
