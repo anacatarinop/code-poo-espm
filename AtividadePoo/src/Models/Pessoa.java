9ed5bf7004930691e2d7624edb5f8b0b6c7a8b34
Artur Cozzi <80040130+ArturCozzi@users.noreply.github.com>
2023-04-13 19:20:22 -0300

    Criar pessoa
    Criar classe pessoa

---
+++ AtividadePoo/src/Models/Pessoa.java
@@ -0,0 +1,28 @@
+ package Models;
+ public abstract class Pessoa {
+   
+   private java.lang.String nome;
+   
+   private java.lang.String cpf;
+   
+   public Pessoa(java.lang.String nome, java.lang.String cpf) {
+   }
+   
+   public java.lang.String getNome() {
+     return null;
+   }
+   
+   public void setNome(java.lang.String nome) {
+   }
+   
+   public java.lang.String getCpf() {
+     return null;
+   }
+   
+   public void setCpf(java.lang.String cpf) {
+   }
+   
+   public java.lang.String toString() {
+     return null;
+   }
+ }
